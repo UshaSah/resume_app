ead1a080630dd95b2a78740b728a27e348e9a559
UshaSah <usah@ucdavis.edu>
2025-08-14 17:43:30 -0700

    style the form container

--- resume_app/src/App.jsx
+++ resume_app/src/App.jsx
@@ -13,37 +13,40 @@ function App() {
         </header>
       </div>
 
-      <div className='form-container'>
-        <h2> General Information</h2>
-        <form>
-          <label for="name">Full Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Your full name"
-            required={true}
-          />
-          <label for="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Your email address"
-            required={true}
-          />
-          <label for="phone">Phone Number</label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            placeholder="123-456-7890"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            title="Please enter a phone number in the format: 123-456-7890"
-            required={true}
+      <div className="form-wrapper">
+        <h1>Profile Information</h1>
+        <div className='form-container'>
+          <h2> General Information</h2>
+          <form>
+            <label for="name">Full Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your full name"
+              required={true}
+            />
+            <label for="email">Email address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your email address"
+              required={true}
+            />
+            <label for="phone">Phone Number</label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              placeholder="123-456-7890"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              title="Please enter a phone number in the format: 123-456-7890"
+              required={true}
 
-          />
-        </form>
+            />
+          </form>
+        </div>
       </div>
     </>
   )
